aabacf56c42489bda428c8a006143fd5a29f423b
Parthipan Natkunam <natkunamparthipan@gmail.com>
2022-04-15 21:40:06 +0200

    create build script with esbuild

---
+++ scripts/bundle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const cssModulesPlugin = require("esbuild-css-modules-plugin");
+const svgrPlugin = require('esbuild-plugin-svgr');
+
+require("esbuild")
+  .build({
+    logLevel: "info",
+    entryPoints: ["src/app/index.js"],
+    inject:["scripts/react-shim.js"],
+    treeShaking: true,
+    bundle: true,
+    outfile: "dist/app.js",
+    plugins: [svgrPlugin(), cssModulesPlugin()],
+    loader: {
+        ".js": "jsx",
+        ".css": "css",
+    },
+    minify: true,
+    target: "es2015",
+    entryNames: "[name]-[hash]",
+    color: true,
+  })
+  .catch(() => process.exit(1))
+  .then(() => {});
